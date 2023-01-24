3709a70b548b71235e59c0d28e22a138b4b5c070
Keith Hetrick <keithjhetrick@gmail.com>
2023-01-24 17:19:33 -0600

    new comp clone

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './views/Home';
-import CreateTask from './components/CreateTask';
-import NavBar from './components/NavBar';
-import UpdateTask from './components/UpdateTask';
-import ViewTask from './components/ViewTask';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import CreateTask from "./components/CreateTask";
+import NavBar from "./components/NavBar";
+import UpdateTask from "./components/UpdateTask";
+import ViewTask from "./components/ViewTask";
+// import "./App.css";
 
 function App() {
-  
   // Task state / status value from Board to Form
-  const [ buttonState, setButtonState ] = useState();
+  const [buttonState, setButtonState] = useState();
 
   return (
     <div className="container">
       <BrowserRouter>
-      <NavBar />
+        <NavBar />
         <Routes>
-          <Route element={ <Home buttonState={ buttonState } setButtonState={ setButtonState } /> } path="/" />
-          <Route element={ <CreateTask buttonState={ buttonState } setButtonState={ setButtonState } /> } path= "/task/create" />
-          <Route element={ <UpdateTask /> } path= "/updatetask/:id" />
-          <Route element={ <ViewTask /> } path= "/task/:id" />
+          <Route
+            element={
+              <Home buttonState={buttonState} setButtonState={setButtonState} />
+            }
+            path="/"
+          />
+          <Route
+            element={
+              <CreateTask
+                buttonState={buttonState}
+                setButtonState={setButtonState}
+              />
+            }
+            path="/task/create"
+          />
+          <Route element={<UpdateTask />} path="/updatetask/:id" />
+          <Route element={<ViewTask />} path="/task/:id" />
         </Routes>
       </BrowserRouter>
     </div>
--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.OUR_PORT;
+const PORT = process.env.OUR_PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
   })
 );
 
@@ -20,4 +20,4 @@ taskRoutes(app);
 const memberRoutes = require("./routes/member.routes");
 memberRoutes(app);
 
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
